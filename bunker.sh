f7ba6792f8e8182e01c077650207b352de55e12d
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-04-18 20:40:06 +0200

    Add files via upload

---
+++ bunker.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+echo " "
+c() {
+exit;bash bunker.sh
+}
+
+e() {
+sleep 0.7;echo "Contraseña incorrecta. ¿No es usted de VW?"
+}
+
+f() {
+S='sleep 0.1'
+N='sleep 0.9'
+echo "¡Bienvenido "$a' ^^'
+echo ' '
+$S
+echo "Esto es el bunker, ahí podrás encontrar contenido de todo tipo^^"
+sleep 6
+$S
+echo "ls..."
+$N
+ES='echo'
+AB=FYZDRYxS#Pm7jwYSI7uaGNdRhhWYCjg
+AC=VRRgRAza#m26QEr17i38Rl7NJS0kQQw
+AD=cU5j2SyC#se4wrxVROyiXttN7-ZZv_g
+AE=0QYyDQzI#c8nfbcv3jIJES4rikh7GMw
+AJ=EIxHAAZJ#wAdQHKSQ2yIl_GOeh6COUA
+AH=sI5XUIIL#TVacjTGRmR99ZqRMVSvQmQ
+AI=pJBmSCjA#erMbiKBIpkUngNc7TLiExw
+AG=sZJUVYIR#rnqKKjRFyjBT69iACFsNQQ
+AK=0MhTVCKZ#5bfe1qw3natJr7fXe-_7DA
+AL=hA51QSbS#oHeqAK9yPCq-txHJ7cNQNQ
+AM=NARySayI#6H72gPVcbV_1ZrCRDQUShQ
+AN=JA5lwCJK#FUTuL0vMK7XkCtVhTmJNIw
+AO=JMA32ITS#xpn9OwfIuO0xVVKB_V_7fg
+AP=xF4ljKyJ#SE71_WRhY_kgDB0_kKearg
+AQ=FAwxWC7R#Yug0MjvftXWktfdjpezSZA
+AR=8chl0Qjb#POe1GaZW6tk409tGOA6aNA
+AS=oQo1iSbT#gy1LzsW4YX8vtS4YHgNkTA
+AT=1EoxgIAA#-nY3RP9Zm9c9LMfxZM_ljw
+AU=BNxDQCqT#jtxqbxzugEbGEDpBLLNG7Q
+clear
+$ES
+$ES
+echo "Inserte una palabra para encontrar sus aportes^^:"
+$L clave
+case $clave in
+web)
+        $ES
+        echo "Aportes relacionados con:" $clave
+        sleep 2
+        $ES
+        $N;echo 'enlace:' $AA$AB
+        $ES
+        $N
+        echo 'enlace:' $AA$AD
+        $ES
+        $N
+        echo 'enlace:' $AA$AE
+        $ES
+        $N
+        echo 'enlace:' $AA$AJ
+        $ES
+        $N
+        echo 'enlace:' $AA$AH
+        ;;
+pentesting | vulnerabilidades | exploits | explotacion | metasploit)
+        $N
+        $ES
+        echo "Aportes relacionados con:" $clave
+        sleep 2
+        $ES
+        $N;echo 'enlace: '$AA$AC
+        $ES
+        $N
+        echo 'enlace:' $AA$AI
+        $ES
+        $N
+        echo 'enlace:' $AA$AG
+        $ES
+        $N
+        echo 'enlace:' $AA$AK
+        ;;
+hacking | wifi | red | redes)
+        $ES
+        $N
+        echo "Aportes relacionados con:" $clave
+        sleep 2
+        $ES
+        $N
+        echo 'enlace:' $AA$AL
+        $ES
+        $N
+        echo 'enlace:' $AA$AM
+        $ES
+        $N
+        echo 'enlace:' $AA$AN
+        $ES
+        $N
+        echo 'enlace:' $AA$AO
+        $ES
+        $N
+        echo 'enlace:' $AA$AP
+        ;;
+programacion | python | programación)
+        $ES
+        $N
+        echo "Aportes relacionados con:" $clave
+        sleep 2
+        $ES
+        $N
+        echo 'enlace:' $AA$AQ
+        $ES
+        $N
+        echo 'enlace:' $AA$AR
+        $ES
+        $N
+        echo 'enlace:' $AA$AS
+        $ES
+        $N
+        echo 'enlace:' $AA$AT
+        $ES
+        $N
+        echo 'enlace:' $AA$AU
+        ;;
+*)
+        $N;echo "Si lees esto, por favor inserte otra palabra clave, la introducida no se encuentra en la lista..."
+        ;;
+esac
+}
+clear
+AF=/folder/
+echo ' '
+echo ' '
+L=read
+BA=OMEGA
+BD=612
+BF=W
+BB=404
+BE=V
+BC=lomax
+BG=_
+echo '( ___ )--------------------------------------( ___ )'
+echo ' | / |                                        | \ |'
+echo ' | / |              #VW APORTES               | \ |'
+echo ' |___|                                        |___|'
+echo '(_____)--------------------------------------(_____)'
+echo '  '
+echo '          ▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭'
+AA=https://mega.nz$AF
+echo '  '
+echo "Por favor inserte la contraseña correcta para hacer uso del script:"
+$L a
+case $a in
+black)
+        c
+        ;;
+blackw)
+        c
+        ;;
+$BA$BB | $BC$BD | $BE$BF | $BE$BF$BG)
+        echo 'Loading...';sleep 1.4
+        echo ' '
+        echo 'Acceso garantizado, bienvenido' $a
+        sleep 3
+        f
+        ;;
+*)
+        e
+        ;;
+esac
